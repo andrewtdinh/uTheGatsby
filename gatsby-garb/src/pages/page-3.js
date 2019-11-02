6db46d25b0d09d5df78b2fc06c4dbc05732ab663
Andrew Dinh <adinh@sightmachine.com>
2019-11-02 10:03:58 -0700

    Build up page 3 with a table of available site images

--- gatsby-garb/src/pages/page-3.js
+++ gatsby-garb/src/pages/page-3.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, useStaticQuery, Link } from 'gatsby';
 import Layout from '../components/layout';
 
+const getImageData = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          relativePath,
+          size,
+          extension,
+          birthTime
+        }
+      }
+    }
+  }
+`
+
 export default () => {
+  const data = useStaticQuery(getImageData);
   return (
     <Layout>
       <h1>Hello from page 3</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Relative Path</th>
+            <th>Size of Image</th>
+            <th>Extension</th>
+            <th>Birthtime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map((edge, index) => (
+            <tr key={index}>
+              <td>{edge.node.relativePath}</td>
+              <td>{edge.node.size}</td>
+              <td>{edge.node.extension}</td>
+              <td>{edge.node.birthTime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <Link to="/page-2">Go to page 2</Link>
     </Layout>
   )
