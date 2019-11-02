1e35db5f6ddafa6ab9463d5f75ad93f082150dee
Andrew Dinh <adinh@sightmachine.com>
2019-11-02 00:03:52 -0700

    Add link to page 2 from page 3

--- gatsby-garb/src/pages/page-3.js
+++ gatsby-garb/src/pages/page-3.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
 export default () => {
   return (
     <Layout>
       <h1>Hello from page 3</h1>
+      <Link to="/page-2">Go to page 2</Link>
     </Layout>
   )
 }
