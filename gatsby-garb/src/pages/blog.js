e1cf7622540dce08b0dc25a9cbf375c9d3d998ee
Andrew Dinh <adinh@sightmachine.com>
2019-11-03 19:15:37 -0800

    Create first blog post based on markdown file

--- gatsby-garb/src/pages/blog.js
+++ gatsby-garb/src/pages/blog.js
@@ -21,14 +21,28 @@ const getMarkdownPosts = graphql`
   }
 `
 
-export default () => {
+export default () => (
   <Layout>
     <div>
       <h1 style={{ display: 'inlineBlock', borderBottom: '1px solid'}}
       >Gatsby Garb Blog</h1>
       <StaticQuery 
-        query={}
+        query={getMarkdownPosts}
+        render={(data) => (
+          <>
+            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <div key={node.id}>
+                <h3>
+                  {node.frontmatter.title} 
+                  <span style={{ color: '#bbb' }}> - {node.frontmatter.date}</span>
+                </h3>
+                <p>{node.excerpt}</p>
+              </div>
+            ))}
+          </>
+        )}
       />
     </div>
   </Layout>
-}
+)
