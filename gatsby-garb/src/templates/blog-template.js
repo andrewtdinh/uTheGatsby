e0d2a1ca2f8b9669f3a1d706dbfc1042d9bb1fd7
Andrew Dinh <adinh@sightmachine.com>
2019-11-22 23:20:45 -0800

    Before building out pagination links

--- gatsby-garb/src/templates/blog-template.js
+++ gatsby-garb/src/templates/blog-template.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, StaticQuery, Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -24,28 +24,50 @@ const getMarkdownPosts = graphql`
   }
 `
 
-export default () => (
-  <Layout>
-    <div>
-      <h1 style={{ display: 'inlineBlock', borderBottom: '1px solid'}}
-      >Gatsby Garb Blog</h1>
-      <StaticQuery 
-        query={getMarkdownPosts}
-        render={(data) => (
-          <>
-            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <div key={node.id}>
-                <h3>
-                  <Link to={`/posts${node.fields.slug}`}>{node.frontmatter.title}</Link> 
-                  <span style={{ color: '#bbb' }}> - {node.frontmatter.date}</span>
-                </h3>
-                <p>{node.excerpt}</p>
-              </div>
-            ))}
-          </>
-        )}
-      />
-    </div>
-  </Layout>
-)
+export default ({ data, pageContext }) => {
+  const { currentPage, isFirstPage, isLastPage } = pageContext;
+  const nextPage = `/blog/${String(currentPage + 1)}`;
+  const prevPage = currentPage - 1 === 1 ? '/blog' : `/blog/${String(currentPage - 1)}`
+
+  return (
+    <Layout>
+      <div>
+        <h1 style={{ display: 'inlineBlock', borderBottom: '1px solid'}}
+        >Gatsby Garb Blog</h1>
+          <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3>
+                <Link to={`/posts${node.fields.slug}`}>{node.frontmatter.title}</Link> 
+                <span style={{ color: '#bbb' }}> - {node.frontmatter.date}</span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </div>
+          ))}
+      </div>
+    </Layout>
+  )}
+
+export const query = graphql`
+  query($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      skip: $skip,
+      limit: $limit
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          id
+          frontmatter {
+            title,
+              date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
