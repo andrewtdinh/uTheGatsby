77682b9644302fc1d4618e997d7f313ae5423e79
Andrew Dinh <adinh@sightmachine.com>
2019-11-26 23:22:13 -0800

    Add time to read for each post

--- gatsby-garb/src/templates/post-template.js
+++ gatsby-garb/src/templates/post-template.js
@@ -7,6 +7,10 @@ export default ({ data: post}) => (
   <Layout>
     <div>
       <h1>{post.markdownRemark.frontmatter.title}</h1>
+      <h4>
+        {post.markdownRemark.timeToRead}{' '}
+        {post.markdownRemark.timeToRead > 1 ? "minutes" : "minute"}
+      </h4>
       <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }}></div>
     </div>
   </Layout>
@@ -17,7 +21,8 @@ export const query = graphql`
     markdownRemark(fields: {
       slug: { eq: $slug}
     }) {
-      html,
+      html
+      timeToRead
       frontmatter {
         title
       }
