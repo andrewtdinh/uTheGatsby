a37e62cbfb2ae57395798b92f44392dbb5a2989e
Andrew Dinh <adinh@sightmachine.com>
2019-11-28 23:38:08 -0800

    Set up contentful plugin with spaceId and access token

--- gatsby-garb/gatsby-config.js
+++ gatsby-garb/gatsby-config.js
@@ -57,7 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
