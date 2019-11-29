db41ab8104a8e1ed0b51e115c63d89dc1e00dbd5
Andrew Dinh <adinh@sightmachine.com>
2019-11-28 23:27:37 -0800

    Install gatsby-source-contentful

--- gatsby-garb/gatsby-config.js
+++ gatsby-garb/gatsby-config.js
@@ -20,6 +20,9 @@ module.exports = {
         plugins: [
           {
             resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
           }
         ]
       }
@@ -45,6 +48,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
