aca3c14fb60fb32cbbc280e9a0793cc668ee6269
Andrew Dinh <adinh@sightmachine.com>
2019-11-29 22:46:57 -0800

    Define product template without query

---
+++ gatsby-garb/src/templates/product-template.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+import Layout from '../components/layout';
+
+const ProductTemplate = () => (
+  <Layout>
+    <div>
+      {/* Product Info */}
+    </div>
+  </Layout>
+)
+
+export default ProductTemplate;
