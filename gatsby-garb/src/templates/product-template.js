0895a7e4dc8005ea735d53305ae40249a08e5f26
Andrew Dinh <adinh@sightmachine.com>
2019-11-29 22:57:56 -0800

    Finish the product template

--- gatsby-garb/src/templates/product-template.js
+++ gatsby-garb/src/templates/product-template.js
@@ -4,12 +4,32 @@ import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 
-const ProductTemplate = () => (
+const ProductTemplate = ({ data: { contentfulProduct } }) => (
   <Layout>
     <div>
       {/* Product Info */}
+      <h2>{contentfulProduct.name} - <span style={{ color: "#ccc"}}
+      >Added on {contentfulProduct.createdAt}</span></h2>
+      <p>{contentfulProduct.description}</p>
+      <Img fluid={contentfulProduct.image.fluid} />
     </div>
   </Layout>
 )
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulProduct(slug: { eq: $slug }) {
+      name
+      price
+      description
+      createdAt(formatString: "MMMM Do, YYYY h:mm:ss a")
+      image {
+        fluid(maxWidth: 800) {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
 
 export default ProductTemplate;
