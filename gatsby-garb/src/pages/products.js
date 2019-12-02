00875f21b21e83920333c9c290aab794db8aa62b
Andrew Dinh <adinh@sightmachine.com>
2019-12-01 22:16:11 -0800

    Add query to the products page

--- gatsby-garb/src/pages/products.js
+++ gatsby-garb/src/pages/products.js
@@ -5,7 +5,28 @@ import Img from 'gatsby-image';
 import Layout from '../components/layout';
 
 const Products = () => (
-
+  <Layout>
+    <div>
+      {/* Product List */}
+    </div>
+  </Layout>
 );
+
+export const query = graphql`
+  allContentfulProduct {
+    edges { 
+      node {
+        id
+        slug
+        name
+        image {
+          fluid(maxWidth: 800) {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Products;
