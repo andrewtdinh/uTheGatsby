d2494568f331bcbf167dd18a132e92bb978d385b
Andrew Dinh <adinh@sightmachine.com>
2019-12-08 11:00:41 -0800

    Fix bug that prevent adding item to cart

--- gatsby-garb/src/templates/product-template.js
+++ gatsby-garb/src/templates/product-template.js
@@ -4,49 +4,52 @@ import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 
-const ProductTemplate = ({ data: { contentfulProduct }, location }) => (
-  <Layout>
-    <div
-      style={{ 
-        marginLeft: "0 auto",
-        width: "100%",
-        textAlign: "center"
-      }}
-    >
-      {/* Product Info */}
-      <div>
-        <h2 style={{ display: "inline-block" }}>
-          {contentfulProduct.name} -
-        </h2>
-        <span style={{ display: "inline-block", color: "#ccc", marginLeft: "4px" }}>
-          Added on {contentfulProduct.createdAt}
-        </span>
-      </div>
-      <h4>${contentfulProduct.price}</h4>
-      <p>{contentfulProduct.description}</p>
-      <button 
-        className="snipcart-add-item"
-        data-item-id={contentfulProduct.slug}
-        data-item-price={contentfulProduct.price}
-        data-item-image={contentfulProduct.image.file.url}
-        data-item-name={contentfulProduct.name}
-        data-item-url={location.pathname}
+const ProductTemplate = ({ data: { contentfulProduct }, location }) => {
+  return (
+    <Layout>
+      <div
+        style={{ 
+          marginLeft: "0 auto",
+          width: "100%",
+          textAlign: "center"
+        }}
       >
-        Add to Cart
-      </button>
-      <Img
-        style={{ margin: "0 auto", maxWidth: '600px' }}
-        fluid={contentfulProduct.image.fluid} 
-      />
-    </div>
-  </Layout>
-)
+        {/* Product Info */}
+        <div>
+          <h2 style={{ display: "inline-block" }}>
+            {contentfulProduct.name} -
+          </h2>
+          <span style={{ display: "inline-block", color: "#ccc", marginLeft: "4px" }}>
+            Added on {contentfulProduct.createdAt}
+          </span>
+        </div>
+        <h4>${contentfulProduct.price}</h4>
+        <p>{contentfulProduct.description}</p>
+        <button 
+          className="snipcart-add-item"
+          data-item-id={contentfulProduct.slug}
+          data-item-price={contentfulProduct.price}
+          data-item-image={contentfulProduct.image.file.url}
+          data-item-name={contentfulProduct.name}
+          data-item-url={location.pathname}
+        >
+          Add to Cart
+        </button>
+        <Img
+          style={{ margin: "0 auto", maxWidth: '600px' }}
+          fluid={contentfulProduct.image.fluid} 
+        />
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query($slug: String!) {
     contentfulProduct(slug: { eq: $slug }) {
       name
       price
+      slug
       description
       createdAt(formatString: "MMMM Do, YYYY h:mm:ss a")
       image {
